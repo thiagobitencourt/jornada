e5492207649db7dc1ce2cc3d43a1b03b640a23d2
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2020-03-26 06:25:58 -0300

    feat: format worked hours on list page

--- src/app/features/list-records/workday-list/workday-list.component.ts
+++ src/app/features/list-records/workday-list/workday-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Workday } from 'src/app/core/model/workday.model';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-workday-list',
@@ -11,4 +12,16 @@ export class WorkdayListComponent implements OnInit {
   
   constructor() {}
   ngOnInit(): void {}
+
+  getTimeFromMins(mins) {
+    let curatedMins = Math.abs(mins);
+    // do not include the first validation check if you want, for example,
+    // getTimeFromMins(1530) to equal getTimeFromMins(90) (i.e. mins rollover)
+    if (curatedMins >= 24 * 60 || curatedMins < 0) {
+        throw new RangeError("Valid input should be greater than or equal to 0 and less than 1440.");
+    }
+    var h = curatedMins / 60 | 0,
+        m = curatedMins % 60 | 0;
+    return moment.utc().hours(h).minutes(m).format("HH:mm");
+  }
 }
