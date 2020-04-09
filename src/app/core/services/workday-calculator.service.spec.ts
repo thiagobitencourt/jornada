214d336fd6811622bb17f268d07c4be616729089
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2020-04-09 06:41:07 -0300

    test: add some more tests for workay calculator service

--- src/app/core/services/workday-calculator.service.spec.ts
+++ src/app/core/services/workday-calculator.service.spec.ts
@@ -12,7 +12,22 @@ fdescribe('WorkdayCalculatorService', () => {
     expect(service).toBeTruthy();
   });
 
+  it('should not change the original workday object', () => {
+    const service: WorkdayCalculatorService = TestBed.get(WorkdayCalculatorService);
+    const workday = {
+      records: [
+        { recordType: RecordType.IN, datetime: new Date('2020-01-01 08:00') },
+        { recordType: RecordType.IN, datetime: new Date('2020-01-01 13:30') },
+        { recordType: RecordType.OUT, datetime: new Date('2020-01-01 12:00') }
+      ]
+    } as Workday;
+
+    service.getTotalWorkday(workday);
+    expect(workday.records[1].datetime).toEqual(new Date('2020-01-01 13:30'));
+  });
+
   it('should calculate the total worked minutes', () => {
+    const service: WorkdayCalculatorService = TestBed.get(WorkdayCalculatorService);
     const workday = {
       records: [
         { recordType: RecordType.IN, datetime: new Date('2020-01-01 08:00') },
@@ -21,17 +36,69 @@ fdescribe('WorkdayCalculatorService', () => {
         { recordType: RecordType.OUT, datetime: new Date('2020-01-01 17:30') }
       ]
     } as Workday;
-    const service: WorkdayCalculatorService = TestBed.get(WorkdayCalculatorService);
     const eigthHoursAsMinutes = 480;
 
     expect(service.getTotalWorkday(workday)).toEqual(eigthHoursAsMinutes);
   });
 
-  it('should ignore a record without a match record (IN without OUT)');
-  it('should ignore a record without a match record (OUT without IN)');
-  it('should not calculate a workday without records');
+  it('should ignore a record without a match record (IN without OUT)', () => {
+    const service: WorkdayCalculatorService = TestBed.get(WorkdayCalculatorService);
+    const halfWorkday = {
+      records: [
+        { recordType: RecordType.IN, datetime: new Date('2020-01-01 08:00') },
+        { recordType: RecordType.IN, datetime: new Date('2020-01-01 13:30') },
+        { recordType: RecordType.OUT, datetime: new Date('2020-01-01 12:00') }
+      ]
+    } as Workday;
+    
+    const invalidWorkday = {
+      records: [
+        { recordType: RecordType.IN, datetime: new Date('2020-01-01 08:00') },
+        { recordType: RecordType.IN, datetime: new Date('2020-01-01 13:30') }
+      ]
+    } as Workday;
 
-  it('should not break when no records list are passed', () => {
+    const fourHoursAsMinutes = 240;
+    const notWorkedAtAll = 0;
+
+    expect(service.getTotalWorkday(halfWorkday)).toEqual(fourHoursAsMinutes);
+    expect(service.getTotalWorkday(invalidWorkday)).toEqual(notWorkedAtAll);
+  });
+
+  it('should ignore a record without a match record (OUT without IN)', () => {
+    const service: WorkdayCalculatorService = TestBed.get(WorkdayCalculatorService);
+    // @TODO: Fix the code. This is considered an invalid workday. But it should be calculated correctly
+    // const halfWorkday = {
+    //   records: [
+    //     { recordType: RecordType.IN, datetime: new Date('2020-01-01 13:30') },
+    //     { recordType: RecordType.OUT, datetime: new Date('2020-01-01 12:00') },
+    //     { recordType: RecordType.OUT, datetime: new Date('2020-01-01 17:30') }
+    //   ]
+    // } as Workday;
+
+    const halfWorkday = {
+      records: [
+        { recordType: RecordType.IN, datetime: new Date('2020-01-01 13:30') },
+        { recordType: RecordType.OUT, datetime: new Date('2020-01-01 17:30') },
+        { recordType: RecordType.OUT, datetime: new Date('2020-01-01 19:30') }
+      ]
+    } as Workday;
+    
+    const invalidWorkday = {
+      records: [
+        { recordType: RecordType.OUT, datetime: new Date('2020-01-01 12:00') },
+        { recordType: RecordType.OUT, datetime: new Date('2020-01-01 17:30') }
+      ]
+    } as Workday;
+
+    const fourHoursAsMinutes = 240;
+    const notWorkedAtAll = 0;
+
+    expect(service.getTotalWorkday(halfWorkday)).toEqual(fourHoursAsMinutes);
+    expect(service.getTotalWorkday(invalidWorkday)).toEqual(notWorkedAtAll);
+  });
+
+  it('should not calculate a workday without records', () => {
     const service: WorkdayCalculatorService = TestBed.get(WorkdayCalculatorService);
     let totalWorkDay = service.getTotalWorkday({} as Workday);
     expect(totalWorkDay).toEqual(0);
--- src/app/core/services/workday-calculator.service.ts
+++ src/app/core/services/workday-calculator.service.ts
@@ -12,9 +12,9 @@ export class WorkdayCalculatorService {
   constructor() {}
 
   getTotalWorkday(workday: Workday): number {
-    const { records = [] } = workday || {};
-    let totalWorkday = 0;
-    
+    const records = [ ...(workday && workday.records || []) ];
+
+    records.sort((recordA, recordB) => recordA.datetime.getTime() - recordB.datetime.getTime());
     const ins = records.filter(({ recordType }) => recordType === RecordType.IN).map(({ datetime }) => datetime);
     const outs = records.filter(({ recordType }) => recordType === RecordType.OUT).map(({ datetime }) => datetime);
 
@@ -26,8 +26,7 @@ export class WorkdayCalculatorService {
       return 0;
     }).reduce((total, time) => (total + time), 0);
 
-    totalWorkday = ((totalWorkdayMilliseconds / 1000) / 60);
-    return totalWorkday;
+    return (totalWorkdayMilliseconds / 1000) / 60;
   }
 
   getOvertime(totalWorkday: number, usedAs: OvertimeType): Overtime {
