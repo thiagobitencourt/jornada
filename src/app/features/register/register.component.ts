f540555196f752b71567000eb0ce501301131ee0
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2021-02-22 07:57:10 -0300

    fix input record modal fields

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { FormGroup, FormBuilder, AbstractControl, Validators } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { WorkdayRecord } from 'src/app/core/model/workday-record.model';
-import { WorkdayService } from 'src/app/core/services/workday.service';
-import { getHours, getMinutes, setHours, setMinutes, setSeconds } from 'date-fns';
-import { UtilService } from 'src/app/core/services/util.service';
+import { Component, OnInit, Inject } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { WorkdayRecord } from "src/app/core/model/workday-record.model";
+import { WorkdayService } from "src/app/core/services/workday.service";
+import {
+  getHours,
+  getMinutes,
+  setHours,
+  setMinutes,
+  setSeconds,
+} from "date-fns";
+import { UtilService } from "src/app/core/services/util.service";
 
 export interface WorkdayFormRecord extends WorkdayRecord {
-  hour: number,
-  minute: number
+  hour: number;
+  minute: number;
 }
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"],
 })
 export class RegisterComponent implements OnInit {
   readonly today = new Date();
@@ -42,13 +48,14 @@ export class RegisterComponent implements OnInit {
       datetime: [initialRecord.datetime, Validators.required],
       hour: [getHours(initialRecord.datetime), Validators.required],
       minute: [getMinutes(initialRecord.datetime), Validators.required],
-      message: [initialRecord.message]
+      message: [initialRecord.message],
     });
   }
 
   saveRecord() {
     this.saving = true;
-    this.workdayService.saveWorkdayRecord(this.getRecordReady())
+    this.workdayService
+      .saveWorkdayRecord(this.getRecordReady())
       .subscribe(() => this.closeDialog())
       .add(() => (this.saving = false));
   }
@@ -58,17 +65,20 @@ export class RegisterComponent implements OnInit {
   }
 
   private getInitialValues(): WorkdayFormRecord {
-    const datetime = this.editingRecord && this.editingRecord.datetime ? new Date(this.editingRecord.datetime) : this.today;
+    const datetime =
+      this.editingRecord && this.editingRecord.datetime
+        ? new Date(this.editingRecord.datetime)
+        : this.today;
     return {
       ...this.editingRecord,
       datetime,
       hour: getHours(datetime),
-      minute: getMinutes(datetime)
+      minute: getMinutes(datetime),
     };
   }
 
   private getRecordReady(): WorkdayRecord {
-    const record: WorkdayFormRecord  = this.timeRecordForm.getRawValue();
+    const record: WorkdayFormRecord = this.timeRecordForm.getRawValue();
 
     record.datetime = setHours(record.datetime, record.hour);
     record.datetime = setMinutes(record.datetime, record.minute);
