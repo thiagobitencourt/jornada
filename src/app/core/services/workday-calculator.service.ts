8e360f8a5ae0040a6658090a334b0b350b348b4c
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2021-02-22 07:56:35 -0300

    Fix total workday presentation

--- src/app/core/services/workday-calculator.service.ts
+++ src/app/core/services/workday-calculator.service.ts
@@ -1,13 +1,11 @@
-import { Injectable } from '@angular/core';
-import { Workday } from '../model/workday.model';
-import { Overtime } from '../model/overtime.model';
-import { OvertimeType } from '../model/overtime-type.enum';
-import { RecordType } from '../model/record-type.enum';
-import { WorkdayRecord } from '../model/workday-record.model';
-import { compareAsc } from 'date-fns';
+import { Injectable } from "@angular/core";
+import { Workday } from "../model/workday.model";
+import { Overtime } from "../model/overtime.model";
+import { OvertimeType } from "../model/overtime-type.enum";
+import { compareAsc, differenceInMinutes } from "date-fns";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class WorkdayCalculatorService {
   fullWorkdayMinutes = 528;
@@ -20,33 +18,36 @@ export class WorkdayCalculatorService {
   }
 
   getTotalWorkday(workday: Workday): number {
-    const records = [ ...(workday && workday.records || []) ];
-    records.sort((recordA, recordB) => compareAsc(new Date(recordA.datetime), new Date(recordB.datetime)));
+    const records = [...((workday && workday.records) || [])];
+    records.sort((recordA, recordB) =>
+      compareAsc(new Date(recordA.datetime), new Date(recordB.datetime))
+    );
 
-    let lastInRecord: WorkdayRecord;
-    const totalWorkdayMilliseconds = records.reduce((total, workdayRecord) => {
-      let workedTime = 0;
-      if (this.isIN(workdayRecord)) {
-        lastInRecord = lastInRecord || workdayRecord;
-      } else if (lastInRecord) {
-        workedTime = workdayRecord.datetime.getTime() - lastInRecord.datetime.getTime();
-        lastInRecord = null;
+    let totalWorkday = 0;
+
+    for (let i = 0; i < records.length - 1; i += 2) {
+      const currentEntry = records[i];
+      const nextEntry = records[i + 1];
+
+      if (nextEntry) {
+        totalWorkday += differenceInMinutes(
+          new Date(nextEntry.datetime),
+          new Date(currentEntry.datetime)
+        );
       }
-      return total + workedTime;
-    }, 0);
+    }
 
-    return (totalWorkdayMilliseconds / 1000) / 60;
+    return totalWorkday;
   }
 
-  getOvertime(totalWorkday: number, usedAs: OvertimeType = OvertimeType.OVERTIME): Overtime {
+  getOvertime(
+    totalWorkday: number,
+    usedAs: OvertimeType = OvertimeType.OVERTIME
+  ): Overtime {
     const overtime = {
       usedAs,
-      total: totalWorkday - this.fullWorkdayMinutes
+      total: totalWorkday - this.fullWorkdayMinutes,
     } as Overtime;
     return overtime;
-  }
-
-  private isIN({ recordType }: WorkdayRecord): boolean {
-    return recordType === RecordType.IN;
   }
 }
--- src/app/features/list-records/workday-list/workday-list.component.ts
+++ src/app/features/list-records/workday-list/workday-list.component.ts
@@ -1,48 +1,53 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Workday } from 'src/app/core/model/workday.model';
-import * as moment from 'moment';
-import { WorkdayRecord } from 'src/app/core/model/workday-record.model';
-import { MatDialog } from '@angular/material/dialog';
-import { RegisterComponent } from '../../register/register.component';
-import { WorkdayService } from 'src/app/core/services/workday.service';
+import { Component, OnInit, Input } from "@angular/core";
+import { Workday } from "src/app/core/model/workday.model";
+import { WorkdayRecord } from "src/app/core/model/workday-record.model";
+import { MatDialog } from "@angular/material/dialog";
+import { RegisterComponent } from "../../register/register.component";
+import { WorkdayService } from "src/app/core/services/workday.service";
 
 @Component({
-  selector: 'app-workday-list',
-  templateUrl: './workday-list.component.html',
-  styleUrls: ['./workday-list.component.scss']
+  selector: "app-workday-list",
+  templateUrl: "./workday-list.component.html",
+  styleUrls: ["./workday-list.component.scss"],
 })
 export class WorkdayListComponent implements OnInit {
   @Input() workdays: Workday[] = [];
-  
-  constructor(public dialog: MatDialog, private workdayService: WorkdayService) {}
+
+  constructor(
+    public dialog: MatDialog,
+    private workdayService: WorkdayService
+  ) {}
   ngOnInit(): void {}
 
   getTimeFromMins(mins) {
-    let curatedMins = Math.abs(mins);
-    // do not include the first validation check if you want, for example,
-    // getTimeFromMins(1530) to equal getTimeFromMins(90) (i.e. mins rollover)
-    if (curatedMins >= 24 * 60 || curatedMins < 0) {
-        throw new RangeError("Valid input should be greater than or equal to 0 and less than 1440.");
-    }
-    var h = curatedMins / 60 | 0,
-        m = curatedMins % 60 | 0;
-    return moment.utc().hours(h).minutes(m).format("HH:mm");
+    const curatedMins = Math.abs(mins);
+    const hours = ((curatedMins / 60) | 0).toString();
+    const minutes = (curatedMins % 60 | 0).toString();
+    return hours.padStart(2, "0") + ":" + minutes.padStart(2, "0");
   }
 
   editRecord(record: WorkdayRecord, workdayIndex: number) {
-    this.dialog.open(RegisterComponent, {
-      width: '40vw',
-      data: { record }
-    }).afterClosed().subscribe(() => {
-      this.workdayService.getUpdatedWorkday(this.workdays[workdayIndex])
-        .subscribe((updatedWorkday: Workday) => {
-          this.workdays[workdayIndex] = { ...this.workdays[workdayIndex], ...updatedWorkday };
-        });
-    });
+    this.dialog
+      .open(RegisterComponent, {
+        width: "40vw",
+        data: { record },
+      })
+      .afterClosed()
+      .subscribe(() => {
+        this.workdayService
+          .getUpdatedWorkday(this.workdays[workdayIndex])
+          .subscribe((updatedWorkday: Workday) => {
+            this.workdays[workdayIndex] = {
+              ...this.workdays[workdayIndex],
+              ...updatedWorkday,
+            };
+          });
+      });
   }
 
   removeRecord(record: WorkdayRecord, workdayIndex: number) {
-    this.workdayService.removerWorkdayRecord(this.workdays[workdayIndex], record)
+    this.workdayService
+      .removerWorkdayRecord(this.workdays[workdayIndex], record)
       .subscribe((newWorkday: Workday) => {
         this.workdays[workdayIndex] = newWorkday;
       });
